44f8a3cad805b6b043d7f4f18b7a34f72fce8754
jayvinb <Jbbusa109@gmail.com>
2023-01-23 10:14:51 +0530

    Initial commit for library book search plugin, adding main plugin file and form for searching books with options for book name, publisher, and price range

---
+++ library-book-search.php
@@ -0,0 +1,10 @@
+<?php
+/*
+* Plugin Name: Library Book Search
+* Description: A plugin for searching library books by name, author, publisher, price, and rating.
+* Version: 1.0
+* Author: Jayvin busa
+* Text Domain: library-book-search
+*/
+
+//Include file for search form and search result
